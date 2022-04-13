d28e54b737ac6d8d2055f8f087285fdcd8a90240
Documenter.jl <documenter@juliadocs.github.io>
2022-04-13 21:52:58 +0000

    build based on 000065d

--- dev/assets/src/codeDevelopment.jl
+++ dev/assets/src/codeDevelopment.jl
@@ -7,12 +7,13 @@ using Plots
 # Continuos target, suppose line segment AB has length 2
 
 #--------------------
-ξ₀=[0.1,0.0]
-α₀ = 0.7;
-#L = collect(0.0:0.01:2.0)
-L = collect(range(1, 2.0, step=0.01))
+ξ₀=[0.0,0.3]
+α₀ = 0.6;
+#L = collect(1.0:0.01:2.0)
+#L = collect(range(1.5, 2.5, step=0.025))
+L = collect(range(1.0, 2.0, step=0.025))
 #L = [1.1,1.5,2.0] # similar to scenario C i.e mutiple targets
-g(k) = ξ₀ .+ k.*[1.0,0.0]
+g(k) = ξ₀ .+ k.*[0.0,1.0]
 temp = quadgk.(g, 0.0, L)
 value = [α₀*(temp[i][1]) for i in 1:length(L)]
 #-------------------Alternate way
@@ -21,21 +22,29 @@ value = [α₀*(temp[i][1]) for i in 1:length(L)]
 p(t) = δ(t-1.0e-15,1.0e-10)
 W=[]
 q = LTIsourcesO(𝐩ₛ, p)
+#for i = 1:length(L)
+#R₁ = LTIsourcesO(ξ₀.+ L[i].*[1.0,0.0], t->q(ξ₀.+ L[i].*[1.0,0.0],t))
+#push!(W,R₁)
+#end
 for i in 1:length(value)
 R₁ = LTIsourcesO(value[i], t->q(value[i],t))
+#z = LTIreceiversO([R₁],𝐩ᵣ)
 push!(W,R₁)
 end
+#z = LTIreceiversO(t->R₁(value[i],t),𝐩ᵣ)
 z = LTIreceiversO(W,𝐩ᵣ)
 
 #TEMPORAL SIMULATION
-t = collect(0.0:1.0e-10:15.5e-9)
-#p1=plot( t, z(t), xlab="time (sec)", ylab="z(t)", legend=:false)
-#display(p1)
+t = collect(0.0:1.0e-10:25.5e-9)
+p1=plot( t, z(t), xlab="time (sec)", ylab="z(t)", legend=:false)
+display(p1)
 
 # Estimator function
 a₁(ξ::Vector{Float64}) = A(distBetween(ξ,𝐩ₛ)./lightSpeed).*A(distBetween(𝐩ᵣ,ξ)./lightSpeed)
 f(ξ::Vector{Float64})=(z((distBetween(ξ,𝐩ₛ) .+ distBetween(𝐩ᵣ,ξ))./lightSpeed))./(a₁(ξ::Vector{Float64}))
-
+T_val1 = map(x->x[1],value)
+T_val2 = map(x->x[2],value)
+line = Any[collect(zip(T_val1,T_val2))]
 #SPATIAL SIMULATION
 Δpos = 0.01
 x_range = collect(-3:Δpos:3)
@@ -43,11 +52,9 @@ y_range = collect(-2:Δpos:2)
 xyGrid = [[x, y] for x in x_range, y in y_range]
 val = [f(𝐮) for 𝐮 ∈ xyGrid]
 p2 = plot(x_range,y_range,transpose(val),st=:surface,camera=(0,90),aspect_ratio=:equal,legend=false,zticks=false,title="Scenario E Simulation")
-scatter!(p2,[𝐩ₛ[1]], [𝐩ₛ[2]],markersize = 8.5,color = :green, marker=:pentagon, label='s' )
-scatter!(p2,[𝐩ᵣ[1]], [𝐩ᵣ[2]],markersize = 3.5,color = :blue, marker=:square, label='r' )
-for i in 1:length(value)
-scatter!(p2,[value[i][1]],[value[i][2]],markersize = 5.5,color = :red, marker=:star8, label='t')
-end
+plot!(p2,line[1],color = :red, lw=5)
+scatter!(p2,[𝐩ₛ[1]], [𝐩ₛ[2]],markersize = 6.5,color = :green, marker=:pentagon, label='s' )
+scatter!(p2,[𝐩ᵣ[1]], [𝐩ᵣ[2]],markersize = 4.5,color = :blue, marker=:square, label='r' )
 display(p2)
 #png(path*"scenarioE_simulation2.png")
 #---------------------------------------------------------------------------
