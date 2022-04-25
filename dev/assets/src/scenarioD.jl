91a21ae2a49c7b1c7fbf6bdefcb035b964f3093e
Documenter.jl <documenter@juliadocs.github.io>
2022-04-25 19:42:17 +0000

    build based on 4ac3bc0

--- dev/assets/src/scenarioD.jl
+++ dev/assets/src/scenarioD.jl
@@ -1,81 +1,55 @@
 path = "docs/src/assets/"
 
-using ISA, LTVsystems
+using LTVsystems
 using Plots
-#Source
 𝐩ₛ =  [0.0, 0.0]
-# Multiple Receiver
 𝐩ᵣ₁ =  [-0.3, 0.0]
 𝐩ᵣ₂ =  [0.0, 0.3]
 𝐩ᵣ₃ =  [0.3, 0.0]
 𝐩ᵣ₄ =  [0.0, -0.3]
-# Transmitter's signal i.e single pulse
-p(t) = δ(t,1.0e-10)
-# Signal observed due to source
+𝐩ᵣ₅ =  [0.0, 0.0]
+p(t) = δn(t,1.0e-10)
 q = LTIsourceO(𝐩ₛ, p)
 #Multiple Targets
 α₁ = 0.7; 𝛏₁ = [0.4,0.7]
-R₁ = LTIsourceO(𝛏₁, t->α₁*q(𝛏₁,t))
 α₂ = 0.5; 𝛏₂ = [0.6,0.2]
-R₂ = LTIsourceO(𝛏₂, t->α₂*q(𝛏₂,t))
-α₃ = 0.4; 𝛏₃ = [0.6,0.9]
-R₃ = LTIsourceO(𝛏₃, t->α₃*q(𝛏₃,t))
+α₃ = 0.4; 𝛏₃ = [0.6,1.0]
+r = pointReflector([𝛏₁,𝛏₂,𝛏₃],[α₁,α₂,α₃],[q])
 # Observed signal
-z₁ = LTIreceiverO([R₁,R₂,R₃],𝐩ᵣ₁)
-z₂ = LTIreceiverO([R₁,R₂,R₃],𝐩ᵣ₂)
-z₃ = LTIreceiverO([R₁,R₂,R₃],𝐩ᵣ₃)
-z₄ = LTIreceiverO([R₁,R₂,R₃],𝐩ᵣ₄)
+z₁ = LTIreceiverO(r,𝐩ᵣ₁)
+z₂ = LTIreceiverO(r,𝐩ᵣ₂)
+z₃ = LTIreceiverO(r,𝐩ᵣ₃)
+z₄ = LTIreceiverO(r,𝐩ᵣ₄)
+z₅ = LTIreceiverO(r,𝐩ᵣ₅)
 t = collect(0.0:1.0e-10:15.5e-9)
 p1 = plot( t, z₁(t), xlab="time (sec)", ylab="z(t)", legend=:false)
 plot!(p1,t, z₂(t))
 plot!(p1,t, z₃(t))
 plot!(p1,t, z₄(t))
+plot!(p1,t, z₅(t))
 
 png(path*"scenarioD_signal.png")
 
+scene2Dplot([q],r,[z₁,z₂,z₃,z₄,z₅])
+
+png(path*"scenarioD.png")
 #----------------------------------------------------
 # Estimator function
-a₁(ξ::Vector{Float64}) = A(distBetween(ξ,𝐩ₛ)./lightSpeed).*A(distBetween(𝐩ᵣ₁,ξ)./lightSpeed)
-a₂(ξ::Vector{Float64}) = A(distBetween(ξ,𝐩ₛ)./lightSpeed).*A(distBetween(𝐩ᵣ₂,ξ)./lightSpeed)
-a₃(ξ::Vector{Float64}) = A(distBetween(ξ,𝐩ₛ)./lightSpeed).*A(distBetween(𝐩ᵣ₃,ξ)./lightSpeed)
-a₄(ξ::Vector{Float64}) = A(distBetween(ξ,𝐩ₛ)./lightSpeed).*A(distBetween(𝐩ᵣ₄,ξ)./lightSpeed)
-f₁(ξ::Vector{Float64})=(z₁((distBetween(ξ,𝐩ₛ) .+ distBetween(𝐩ᵣ₁,ξ))./lightSpeed))./(a₁(ξ::Vector{Float64}))
-f₂(ξ::Vector{Float64})=(z₂((distBetween(ξ,𝐩ₛ) .+ distBetween(𝐩ᵣ₂,ξ))./lightSpeed))./(a₂(ξ::Vector{Float64}))
-f₃(ξ::Vector{Float64})=(z₃((distBetween(ξ,𝐩ₛ) .+ distBetween(𝐩ᵣ₃,ξ))./lightSpeed))./(a₃(ξ::Vector{Float64}))
-f₄(ξ::Vector{Float64})=(z₄((distBetween(ξ,𝐩ₛ) .+ distBetween(𝐩ᵣ₄,ξ))./lightSpeed))./(a₄(ξ::Vector{Float64}))
+f₁(ξ::Vector{Float64})=(z₁((norm(ξ-𝐩ₛ) .+ norm(𝐩ᵣ₁-ξ))./c))./A(norm(ξ-𝐩ₛ)./c).*A(norm(𝐩ᵣ₁-ξ)./c)
+f₂(ξ::Vector{Float64})=(z₂((norm(ξ-𝐩ₛ) .+ norm(𝐩ᵣ₂-ξ))./c))./A(norm(ξ-𝐩ₛ)./c).*A(norm(𝐩ᵣ₂-ξ)./c)
+f₃(ξ::Vector{Float64})=(z₃((norm(ξ-𝐩ₛ) .+ norm(𝐩ᵣ₃-ξ))./c))./A(norm(ξ-𝐩ₛ)./c).*A(norm(𝐩ᵣ₃-ξ)./c)
+f₄(ξ::Vector{Float64})=(z₄((norm(ξ-𝐩ₛ) .+ norm(𝐩ᵣ₄-ξ))./c))./A(norm(ξ-𝐩ₛ)./c).*A(norm(𝐩ᵣ₄-ξ)./c)
+f₅(ξ::Vector{Float64})=(z₅((norm(ξ-𝐩ₛ) .+ norm(𝐩ᵣ₅-ξ))./c))./A(norm(ξ-𝐩ₛ)./c).*A(norm(𝐩ᵣ₅-ξ)./c)
 
-f(ξ::Vector{Float64})=f₁(ξ::Vector{Float64}).+f₂(ξ::Vector{Float64}).+f₃(ξ::Vector{Float64}).+f₄(ξ::Vector{Float64})
+f(ξ::Vector{Float64})=f₁(ξ::Vector{Float64}).+f₂(ξ::Vector{Float64}).+f₃(ξ::Vector{Float64}).+f₄(ξ::Vector{Float64}).+f₅(ξ::Vector{Float64})
 #SPATIAL SIMULATION3
-Δpos = 0.01
-x_range = collect(-3:Δpos:3)
-y_range = collect(-2:Δpos:2)
-xyGrid = [[x, y] for x in x_range, y in y_range]
-val = [f(𝐮) for 𝐮 ∈ xyGrid]
-
-p2 = plot(x_range,y_range,transpose(val),st=:surface,camera=(0,90),aspect_ratio=:equal,legend=true,zticks=false,bg = RGB(0.1, 0.1, 0.1))
-scatter!(p2,[𝐩ₛ[1]], [𝐩ₛ[2]],markersize = 6.5,color = :green, marker=:pentagon, label='s')
-scatter!(p2,[𝐩ᵣ₁[1]], [𝐩ᵣ₁[2]],markersize = 5.5,color = :blue, marker=:square, label='r')
-scatter!(p2,[𝐩ᵣ₂[1]], [𝐩ᵣ₂[2]],markersize = 5.5,color = :blue, marker=:square, label='r')
-scatter!(p2,[𝐩ᵣ₃[1]], [𝐩ᵣ₃[2]],markersize = 5.5,color = :blue, marker=:square, label='r')
-scatter!(p2,[𝐩ᵣ₄[1]], [𝐩ᵣ₄[2]],markersize = 5.5,color = :blue, marker=:square, label='r')
-scatter!(p2,[𝛏₁[1]],[𝛏₁[2]],markersize = 8.5,color = :red, marker=:star8, label='t')
-scatter!(p2,[𝛏₂[1]],[𝛏₂[2]],markersize = 8.5,color = :red, marker=:star8, label='t')
-scatter!(p2,[𝛏₃[1]],[𝛏₃[2]],markersize = 8.5,color = :red, marker=:star8, label='t')
-
-
+inverse2Dplot([q],r,[z₁,z₂,z₃,z₄,z₅],f;x_min = -3.0,x_max = 3.0,y_min = -2.0,y_max = 2.0)
 png(path*"scenarioD_simulation.png")
 
 # Target estimation
-f_new(ξ::Vector{Float64})=(f₁(ξ::Vector{Float64}).*f₂(ξ::Vector{Float64}).*f₃(ξ::Vector{Float64}).*f₄(ξ::Vector{Float64}))^(1/3)
+f_new(ξ::Vector{Float64})=(f₁(ξ::Vector{Float64}).*f₂(ξ::Vector{Float64}).*f₃(ξ::Vector{Float64}).*f₄(ξ::Vector{Float64}).*f₅(ξ::Vector{Float64}))^(1/3)
 #SPATIAL SIMULATION
-val1 = [f_new(𝐮) for 𝐮 ∈ xyGrid]
-p3 = plot(x_range,y_range,transpose(val1),st=:surface,camera=(0,90),aspect_ratio=:equal,legend=true,zticks=false,bg = RGB(0.1, 0.1, 0.1))
-#scatter!(p3,[𝐩ₛ[1]], [𝐩ₛ[2]],markersize = 8.5,color = :green, marker=:pentagon, label='s' )
-#scatter!(p3,[𝐩ᵣ1[1]], [𝐩ᵣ1[2]],markersize = 5.5,color = :blue, marker=:square, label='r')
-#scatter!(p3,[𝐩ᵣ2[1]], [𝐩ᵣ2[2]],markersize = 5.5,color = :blue, marker=:square, label='r' )
-#scatter!(p3,[𝛏₁[1]],[𝛏₁[2]],markersize = 8.5,color = :red, marker=:star8, label='t')
-#scatter!(p3,[𝛏₂[1]],[𝛏₂[2]],markersize = 8.5,color = :red, marker=:star8, label='t')
-
+inverse2Dplot([q],r,[z₁,z₂,z₃,z₄,z₅],f_new;Δpos = 0.01,x_min = -3.0,x_max = 3.0,y_min = -2.0,y_max = 2.0,)
 png(path*"scenarioD_target_estimation.png")
 #-----------------------------------------------------------------
 # with 3 target and 3 receiver
@@ -102,12 +76,12 @@ plot!(p1,t, z₂(t))
 plot!(p1,t, z₃(t))
 
 #-----------------------------------------
-a₁(ξ::Vector{Float64}) = (α₁.+α₂.+α₃).*A(distBetween(ξ,𝐩ₛ)./lightSpeed).*A(distBetween(𝐩ᵣ1,ξ)./lightSpeed)
-a₂(ξ::Vector{Float64}) = (α₁.+α₂.+α₃).*A(distBetween(ξ,𝐩ₛ)./lightSpeed).*A(distBetween(𝐩ᵣ2,ξ)./lightSpeed)
-a₃(ξ::Vector{Float64}) = (α₁.+α₂.+α₃).*A(distBetween(ξ,𝐩ₛ)./lightSpeed).*A(distBetween(𝐩ᵣ3,ξ)./lightSpeed)
-f₁(ξ::Vector{Float64})=(z₁((distBetween(ξ,𝐩ₛ) .+ distBetween(𝐩ᵣ1,ξ))./lightSpeed))./(a₁(ξ::Vector{Float64}))
-f₂(ξ::Vector{Float64})=(z₂((distBetween(ξ,𝐩ₛ) .+ distBetween(𝐩ᵣ2,ξ))./lightSpeed))./(a₂(ξ::Vector{Float64}))
-f₃(ξ::Vector{Float64})=(z₃((distBetween(ξ,𝐩ₛ) .+ distBetween(𝐩ᵣ3,ξ))./lightSpeed))./(a₃(ξ::Vector{Float64}))
+a₁(ξ::Vector{Float64}) = (α₁.+α₂.+α₃).*A(norm(ξ-𝐩ₛ)./c).*A(distBetween(𝐩ᵣ1,ξ)./c)
+a₂(ξ::Vector{Float64}) = (α₁.+α₂.+α₃).*A(norm(ξ-𝐩ₛ)./c).*A(distBetween(𝐩ᵣ2,ξ)./c)
+a₃(ξ::Vector{Float64}) = (α₁.+α₂.+α₃).*A(norm(ξ-𝐩ₛ)./c).*A(distBetween(𝐩ᵣ3,ξ)./c)
+f₁(ξ::Vector{Float64})=(z₁((norm(ξ-𝐩ₛ) .+ distBetween(𝐩ᵣ1,ξ))./c))./(a₁(ξ::Vector{Float64}))
+f₂(ξ::Vector{Float64})=(z₂((norm(ξ-𝐩ₛ) .+ distBetween(𝐩ᵣ2,ξ))./c))./(a₂(ξ::Vector{Float64}))
+f₃(ξ::Vector{Float64})=(z₃((norm(ξ-𝐩ₛ) .+ distBetween(𝐩ᵣ3,ξ))./c))./(a₃(ξ::Vector{Float64}))
 f(ξ::Vector{Float64})=f₁(ξ::Vector{Float64}).+f₂(ξ::Vector{Float64}).+
 f₃(ξ::Vector{Float64})
 Δpos = 0.01
