6431194a407fc544fdd2ebd3cc6d61c841c93a40
Documenter.jl <documenter@juliadocs.github.io>
2022-04-28 21:43:25 +0000

    build based on b194002

--- dev/assets/src/scenarioE.jl
+++ dev/assets/src/scenarioE.jl
@@ -8,8 +8,8 @@ using Plots
 𝐩ₛ₂ =  [0.1, 0.0]
 𝐩ᵣ₂ =  [0.5, 0.0]
 
-𝐩ₛ₃ =  [0.9, 0.0]
-𝐩ᵣ₃ =  [1.3, 0.0]
+𝐩ₛ₃ =  [0.8, 0.0]
+𝐩ᵣ₃ =  [1.2, 0.0]
 
 p(t) = δn(t,1.0e-10)
 q₁ = LTIsourceO(𝐩ₛ₁, p)
@@ -28,15 +28,16 @@ z₁ = LTIreceiverO([r₁],𝐩ᵣ₁)
 z₂ = LTIreceiverO([r₂],𝐩ᵣ₂)
 z₃ = LTIreceiverO([r₃],𝐩ᵣ₃)
 
+t = collect(0.0:1.0e-10:15.5e-9)
+p1 = plot( t, z₁(t), xlab="time (sec)", ylab="z(t)", legend=:false)
+plot!(p1,t, z₂(t))
+plot!(p1,t, z₃(t))
 
-#z₁ = LTIreceiverO([r₁,r₂,r₃],𝐩ᵣ₁)
-#z₂ = LTIreceiverO([r₁,r₂,r₃],𝐩ᵣ₂)
-#z₃ = LTIreceiverO([r₁,r₂,r₃],𝐩ᵣ₃)
 
 
 png(path*"scenarioE_signal.png")
 
-scene2Dplot([q₁,q₂,q₃],[r₁,r₂,r₃],[z₁,z₂,z₃,z])
+scene2Dplot([q₁,q₂,q₃],[r₁,r₂,r₃],[z₁,z₂,z₃])
 
 png(path*"scenarioE.png")
 
@@ -45,20 +46,23 @@ f₂(ξ::Vector{Float64})=(z₂((norm(ξ-𝐩ₛ₂) .+ norm(𝐩ᵣ₂-ξ))./c)
 f₃(ξ::Vector{Float64})=(z₃((norm(ξ-𝐩ₛ₃) .+ norm(𝐩ᵣ₃-ξ))./c))./(A(norm(ξ-𝐩ₛ₃)./c).*A(norm(𝐩ᵣ₃-ξ)./c))
 
 
-f₁(ξ::Vector{Float64})=(z((norm(ξ-𝐩ₛ₁) .+ norm(𝐩ᵣ₁-ξ))./c))./(A(norm(ξ-𝐩ₛ₁)./c).*A(norm(𝐩ᵣ₁-ξ)./c))
-f₂(ξ::Vector{Float64})=(z((norm(ξ-𝐩ₛ₂) .+ norm(𝐩ᵣ₂-ξ))./c))./(A(norm(ξ-𝐩ₛ₂)./c).*A(norm(𝐩ᵣ₂-ξ)./c))
-f₃(ξ::Vector{Float64})=(z((norm(ξ-𝐩ₛ₃) .+ norm(𝐩ᵣ₃-ξ))./c))./(A(norm(ξ-𝐩ₛ₃)./c).*A(norm(𝐩ᵣ₃-ξ)./c))
-
-
-
 f(ξ::Vector{Float64})=f₁(ξ::Vector{Float64}).+f₂(ξ::Vector{Float64}).+f₃(ξ::Vector{Float64})
-inverse2Dplot([q₁,q₂,q₃],[r₁,r₂,r₃],[z₁,z₂,z₃,z],f;x_min = -3.0,x_max = 3.0,y_min = -2.0,y_max = 2.0)
+inverse2Dplot([q₁,q₂,q₃],[r₁,r₂,r₃],[z₁,z₂,z₃],f;x_min = -3.0,x_max = 3.0,y_min = -2.0,y_max = 2.0)
 
 png(path*"scenarioE_simulation.png")
 
 # Target estimation
-f_new(ξ::Vector{Float64})=(f₁(ξ::Vector{Float64}).*f₂(ξ::Vector{Float64}).*f₃(ξ::Vector{Float64}))^(1/3)
+f_new(ξ::Vector{Float64})=(f₁(ξ::Vector{Float64}).*f₂(ξ::Vector{Float64}).*f₃(ξ::Vector{Float64}))
 #SPATIAL SIMULATION
-inverse2Dplot([q₁,q₂,q₃],r,[z₁,z₂,z₃],f_new;Δpos = 0.01,x_min = -3.0,x_max = 3.0,y_min = -2.0,y_max = 2.0,)
+inverse2Dplot([q₁,q₂,q₃],[r₁,r₂,r₃],[z₁,z₂,z₃],f_new;Δpos = 0.01,x_min = -3.0,x_max = 3.0,y_min = -2.0,y_max = 2.0,)
 
-png(path*"scenarioD_target_estimation.png")
+png(path*"scenarioE_target_estimation.png")
+
+
+#Δpos = 0.01
+#x_range = collect(-3:Δpos:3)
+#y_range = collect(-3:Δpos:3)
+#xyGrid = [[x, y] for x in x_range, y in y_range]
+#val11 = [f_new(𝐮) for 𝐮 ∈ xyGrid]
+#plot(x_range,y_range,transpose(val11),st=:surface,camera=(0,90),
+#         aspect_ratio=:equal,legend=:outertopright,colorbar=false,zticks=false)
