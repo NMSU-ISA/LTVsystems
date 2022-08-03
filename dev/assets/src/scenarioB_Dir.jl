2c9cc4840a1e42e0ca82205776cf17bfe88bc263
Documenter.jl <documenter@juliadocs.github.io>
2022-08-03 02:31:36 +0000

    build based on 64c5bbb

--- dev/assets/src/scenarioB_Dir.jl
+++ dev/assets/src/scenarioB_Dir.jl
@@ -7,29 +7,28 @@ using Plots
 p(t) = δn(t,1.0e-10)
 𝐛 = [1.0,0.0]
 G(θ) = 𝒩ᵤ(θ, μ=0.0, σ=π/8)
-q = LTIsourceDTI(𝐩ₛ,p,𝐛,G)0000
+q = LTIsourceDTI(𝐩ₛ,p,𝐛,G)
 α₀ = 0.7; 𝛏₀ = [1.8,0.0]
 r = pointReflector(𝛏₀,α₀,q)
 z = LTIreceiverDTI([r],𝐩ᵣ,𝐛,G)
 #TEMPORAL SIMULATION
-t = collect(0.0:1.0e-10:15.5e-9)
 plot( t, z(t), xlab="time (sec)", ylab="z(t)", legend=:false)
 
 
-png(path*"scenarioBLTIDir_signal.png")
+png(path*"scenarioB_LTIDirsignal.png")
 
 scene2Dplot([q],[r],[z])
 
-png(path*"scenarioBLTIDir.png")
+png(path*"scenarioB_LTIDir.png")
 
 # Inverse modeling
 
 Dᵣ(ξ::Vector{Float64}) = G(angleBetween(𝐛, ξ.-𝐩ᵣ))
 Dₛ(ξ::Vector{Float64}) = G(angleBetween(𝐛, ξ.-𝐩ₛ))
-f(ξ::Vector{Float64}) = z((norm(ξ-𝐩ₛ) .+ norm(𝐩ᵣ-ξ))./c).*Dᵣ(ξ::Vector{Float64}).*Dₛ(ξ::Vector{Float64})/
+f(ξ::Vector{Float64}) = z((norm(ξ-𝐩ₛ) .+ norm(𝐩ᵣ-ξ))./c).*Dₛ(ξ::Vector{Float64}).*Dᵣ(ξ::Vector{Float64})/
                         (A(norm(ξ-𝐩ₛ)/c).*A(norm(𝐩ᵣ-ξ)/c))
 
 #SPATIAL SIMULATION
 inverse2Dplot([q],[r],[z],f)
 
-png(path*"scenarioA_DirTIsimulation.png")
+png(path*"scenarioB_DirTIsimulation.png")
