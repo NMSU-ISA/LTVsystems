4e656b22f3eeb04e9de3cbf0272661da6c6855d0
Mamta Dalal <mamta.dalal7@gmail.com>
2022-09-14 17:33:19 -0600

    added scenario D directionalLTI

--- test/testDoppler.jl
+++ test/testDoppler.jl
@@ -59,7 +59,7 @@ using LTVsystems
 using Plots
 𝐩ₛ = [0.0, 0.0]
 𝐩ᵣ(t) = [0.0, 0.0] + [1.0, 0.0]*t
-p(t) = exp(im*2π*2.0e9*t)
+p(t) = exp(im*2π*1.0e9*t)
 #p(t) = exp(im*2π*1.0e09*t)
 q = LTIsourceO(𝐩ₛ, p)
 α₀ = 0.7; 𝛏₀ = [1.8,0.0]
@@ -67,5 +67,5 @@ r = pointReflector(𝛏₀,α₀,q)
 z = LTVreceiverO([r,q],𝐩ᵣ)
 #TEMPORAL SIMULATION
 #t = collect(0.0:1.0e-10:25.5e-9)
-t = collect(0.0:0.01:5.0)
+t = collect(-5.0:0.01:5.0)
 plot(t,z(t), xlab="time (sec)", ylab="z(t)", legend=:false)
