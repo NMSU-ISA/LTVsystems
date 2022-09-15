80e1a7a01dee5906141c5bbd27ad17810a9215f0
Steven Sandoval <ssandova@gmail.com>
2022-09-14 18:01:46 -0600

    example

--- src/codeDevelopment.jl
+++ src/codeDevelopment.jl
@@ -1,17 +1,17 @@
 using Roots, LinearAlgebra, LTVsystems, Plots
 
-𝐩ₛ(t) = [1.0c, 1.0c] + [0.5c, 1.0]*t
+𝐩ₛ(t) = [1.0c, 1.0c] + [0.9c, 1.0]*t
 𝛏₀ = [1.0c, 0.0]
 
-p(t) = exp(-t^2)*exp(1im*2π*5*t)
-p(t) = exp(-t^2)
+p(t) = exp(1im*2π*5*t)
+#p(t) = exp(-t^2)
 
 q = LTVsourceO(𝐩ₛ,p)
 
 t₀ = 0.0
 q(𝛏₀,t₀)
 
-t = collect(0.0:0.01:5.0)
+t = collect(-3.0:0.001:3.0)
 z = [ q(𝛏₀,t₀) for t₀∈t] 
 
 plot(t,real(p.(t)))
