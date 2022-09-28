e4bf952406323ac3628fb358c6143b602efdd862
Mamta Dalal <mamta.dalal7@gmail.com>
2022-09-27 18:04:48 -0600

    edit STAT example

--- docs/src/assets/src/sceneSTATD.jl
+++ docs/src/assets/src/sceneSTATD.jl
@@ -2,29 +2,93 @@ path = "docs/src/assets/"
 
 using LTVsystems
 using Plots
-𝐩ₛ = [0.3, 0.3]
-𝐩ᵣ = [0.9, 0.9]
-T  = 15.0e-09
-p(t) = δn(t,2.0e-10) + δn(t-T,2.0e-10) + δn(t-2T,2.0e-10)
-α₁ = 0.7; 𝛏₁ = [1.2,0.0]
-α₂ = 0.6; 𝛏₂ = [1.8,1.8]
-α₃ = 0.5; 𝛏₃ = [2.7,-0.9]
+𝐩ₛ = [0.1c, 0.0]
+𝐩ᵣ = [0.11c, 0.02c]
+T  = 10
+p(t) = δn(t-0.5,0.1) + δn(t-0.5-T,0.1) + δn(t-0.5-2T,0.1)
 
-ω = T/3
+α₁ = 0.7; 𝛏₁ = [0.12c,0.0] 
+#α₂ = 0.6; 𝛏₂ = [0.5c,0.5c]
+#α₃ = 0.5; 𝛏₃ = [0.7c,0.2c]
+
+ω = T*1.0e09/3
+
 𝐛(t) = [cos(2π*ω*t), sin(2π*ω*t)]
 G(θ) = 𝒩ᵤ(θ, μ=0.0, σ=π/4)
-q = STATsourceD(𝐩ₛ,p,𝐛,G)
-r = pointReflector([𝛏₁,𝛏₂,𝛏₃],[α₁,α₂,α₃],[q])
-z = STATreceiverD(r,𝐩ᵣ,𝐛,G)
 
-t = collect(-10.5e-9:1.0e-10:50.5e-9)
+q = STATsourceD(𝐩ₛ,p,𝐛,G)
+#r = pointReflector([𝛏₁,𝛏₂,𝛏₃],[α₁,α₂,α₃],[q])
+#z = STATreceiverD(r,𝐩ᵣ,𝐛,G)
+r = pointReflector(𝛏₁,α₁,q)
+z = STATreceiverD([r],𝐩ᵣ,𝐛,G)
+
+t = 0.0:0.1:35
 p1 = plot(t,p, xlab="time (sec)", ylab="p(t)", legend=:false)
 p2 = plot( t, z(t), xlab="time (sec)", ylab="z(t)", legend=:false)
 plot(p1,p2,layout=(2,1))
 
+scene2Dplot([q],[r],[z])
+#plot(t,p)
+
+# when time in nanoseconds
+
+using LTVsystems
+using Plots
+𝐩ₛ = [0.1, 0.0]
+𝐩ᵣ = [0.4, 0.2]
+T  = 10.0e-9
+p(t) = δn(t-0.5e-9,1.0e-10) + δn(t-0.5e-9-T,1.0e-10) + δn(t-0.5e-9-2T,1.0e-10)
+
+α₁ = 0.7; 𝛏₁ = [0.8,0.0]
+#α₂ = 0.6; 𝛏₂ = [0.5,0.5]
+#α₃ = 0.5; 𝛏₃ = [0.7,0.2]
+
+ω = T/3
+𝐛(t) = [cos(2π*ω*t), sin(2π*ω*t)]
+G(θ) = 𝒩ᵤ(θ, μ=0.0, σ=π/4)
+
+q = STATsourceD(𝐩ₛ,p,𝐛,G)
+#r = pointReflector([𝛏₁,𝛏₂,𝛏₃],[α₁,α₂,α₃],[q])
+#z = STATreceiverD(r,𝐩ᵣ,𝐛,G)
+r = pointReflector(𝛏₁,α₁,q)
+z = STATreceiverD([r],𝐩ᵣ,𝐛,G)
+
+t = 0.0:1.0e-10:35.0e-9
+p1 = plot(t,p, xlab="time (sec)", ylab="p(t)", legend=:false)
+p2 = plot( t, z(t), xlab="time (sec)", ylab="z(t)", legend=:false)
+plot(p1,p2,layout=(2,1))
+
+
+scene2Dplot([q],[r],[z])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 png(path*"scenarioD_STATDsignal1.png")
 
 #Inverse modeling
+
 Dᵣ(ξ::Vector{Float64}) = G(angleBetween(𝐛((norm(ξ-𝐩ₛ) .+ norm(𝐩ᵣ-ξ))./c), ξ.-𝐩ᵣ))
 Dₛ(ξ::Vector{Float64}) = G(angleBetween(𝐛((norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c), ξ.-𝐩ₛ))
 
@@ -34,11 +98,11 @@ zt=NewSources(z,T)
 znew = PulseTrainReceivers(z,T)
 
 
-f(ξ::Vector{Float64}) = (znew((norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c).*Dₛ(ξ).*Dᵣ(ξ))/
-                           (A(norm(ξ-𝐩ₛ)/c).*A(norm(𝐩ᵣ-ξ)/c))
+f(ξ::Vector{Float64}) = (znew((norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c).*Dᵣ(ξ).*Dₛ(ξ))/
+                        (A(norm(ξ-𝐩ₛ)/c).*A(norm(𝐩ᵣ-ξ)/c))
 
 
-inverse2Dplot([q],r,[z],f,Δpos = 0.01,x_min = -5.0,x_max = 5.0,y_min = -5.0,y_max = 5.0)
+inverse2Dplot([q],[r],[z],f,Δpos = 0.01,x_min = -5.0,x_max = 5.0,y_min = -5.0,y_max = 5.0)
 
 png(path*"scenarioD_STATDsimulationnew.png")
 
