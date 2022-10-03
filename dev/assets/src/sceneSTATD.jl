527888333b47980eab7c9824dc7d7de48d528c10
Documenter.jl <documenter@juliadocs.github.io>
2022-10-03 06:54:19 +0000

    build based on 0339f7c

--- dev/assets/src/sceneSTATD.jl
+++ dev/assets/src/sceneSTATD.jl
@@ -7,7 +7,7 @@ using Plots
 T  = 10
 p(t) = δn(t-0.5,0.1) + δn(t-0.5-T,0.1) + δn(t-0.5-2T,0.1)
 
-α₁ = 0.7; 𝛏₁ = [0.12c,0.0] 
+α₁ = 0.7; 𝛏₁ = [0.12c,0.0]
 #α₂ = 0.6; 𝛏₂ = [0.5c,0.5c]
 #α₃ = 0.5; 𝛏₃ = [0.7c,0.2c]
 
@@ -38,13 +38,13 @@ using Plots
 𝐩ᵣ = [0.4, 0.2]
 T  = 20.0e-9
 p(t) = δn(t-0.5e-9,1.0e-10) + δn(t-0.5e-9-T,1.0e-10) + δn(t-0.5e-9-2T,1.0e-10)
+#Reflectors
+α₁ = 0.7; 𝛏₁ = [0.8,0.0]
+α₂ = 0.6; 𝛏₂ = [1.0,1.0]
+α₃ = 0.5; 𝛏₃ = [1.5,0.2]
 
-α₁ = 0.7; 𝛏₁ = [1.8,0.0]
-α₂ = 0.6; 𝛏₂ = [1.1,1.1]
-α₃ = 0.5; 𝛏₃ = [2.0,-0.2] 
-
-ω = T/3
-𝐛(t) = [cos(2π*ω*t), sin(2π*ω*t)]
+f₀ = 1/3T
+𝐛(t) = [cos(2π*f₀*t),sin(2π*f₀*t)]
 G(θ) = 𝒩ᵤ(θ, μ=0.0, σ=π/4)
 
 q = STATsourceD(𝐩ₛ,p,𝐛,G)
@@ -53,7 +53,7 @@ z = STATreceiverD(r,𝐩ᵣ,𝐛,G)
 #r = pointReflector(𝛏₁,α₁,q)
 #z = STATreceiverD([r],𝐩ᵣ,𝐛,G)
 
-t = 0.0:1.0e-10:55.0e-9
+t = -5.0e-9:1.0e-10:75.0e-9
 p1 = plot(t,p, xlab="time (sec)", ylab="p(t)", legend=:false)
 p2 = plot( t, z(t), xlab="time (sec)", ylab="z(t)", legend=:false)
 plot(p1,p2,layout=(2,1))
@@ -65,17 +65,18 @@ png(path*"scenarioD_STATDsignal1.png")
 
 #Inverse modeling
 
-Dᵣ(ξ::Vector{Float64}) = G(angleBetween(𝐛((norm(ξ-𝐩ₛ) .+ norm(𝐩ᵣ-ξ))./c), ξ.-𝐩ᵣ))
-Dₛ(ξ::Vector{Float64}) = G(angleBetween(𝐛((norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c), ξ.-𝐩ₛ))
+function beam(t::Float64)
+    return ifelse(0.0<t<T,𝐛(t) , ifelse(T<t<2T, 𝐛(T+t), ifelse(2T<t<3T, 𝐛(2T+t), 𝐛(3T+t))))
+end
 
 
-#znew = PulseTrainReceivers(z,T)
+
+Dₛ(ξ::Vector{Float64}) = G(angleBetween(beam(norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c, ξ.-𝐩ₛ))
+
 zₜ = PulseTrainReceivers(z,T)
 
-f(ξ::Vector{Float64}) = (zₜ((norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c).*Dᵣ(ξ).*Dₛ(ξ))/
+f(ξ::Vector{Float64}) = (zₜ((norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c).*Dₛ(ξ))/
                         (A(norm(ξ-𝐩ₛ)/c).*A(norm(𝐩ᵣ-ξ)/c))
-
-
 inverse2Dplot([q],r,[z],f)
 
 png(path*"scenarioD_STATDsimulationnew.png")
