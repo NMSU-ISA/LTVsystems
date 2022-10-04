9a4959ab82eac20ab29fdfa4121517a76e0206dc
Documenter.jl <documenter@juliadocs.github.io>
2022-10-04 19:37:37 +0000

    build based on 128963e

--- dev/assets/src/scenarioE_statDir.jl
+++ dev/assets/src/scenarioE_statDir.jl
@@ -6,34 +6,16 @@ using Plots
 𝐩ᵣ = [0.0, 0.0]
 T  = 15.0e-9
 p(t) = δn(t-0.5e-9,1.0e-10) + δn(t-0.5e-9-T,1.0e-10) + δn(t-0.5e-9-2T,1.0e-10)+ δn(t-0.5e-9-3T,1.0e-10)
-
-#Reflectors
-α₁ = 0.7; 𝛏₁ = [1.0,0.0]
-α₂ = 0.6; 𝛏₂ = [-1.0,0.0]
-α₃ = 0.6; 𝛏₃ = [0.0,1.0]
-α₄ = 0.5; 𝛏₄ = [0.0,-1.0]
-
-#ω = T/4
+α₁ = 0.7; 𝛏₁ = [2.0,0.0]
+α₂ = 0.7; 𝛏₂ = [-2.0,0.0]
+α₃ = 0.7; 𝛏₃ = [0.0,2.0]
+α₄ = 0.7; 𝛏₄ = [0.0,-2.0]
 f₀ = 1/4T
 𝐛(t) = [cos(2π*f₀*t),sin(2π*f₀*t)]
-#𝐛(t) = 𝐛(t)/norm(𝐛(t))
-#t = 0.0:1.0e-10:4T
-#plot(t,getindex.(𝐛.(t),1))
-
-function beam(t::Float64)
-    return ifelse(0.0<t<T,𝐛(t) , ifelse(T<t<2T, 𝐛(t-T), ifelse(2T<t<3T, 𝐛(t-2T), 𝐛(t-3T))))
-end
-
-G(θ) = 𝒩ᵤ(θ, μ=0.0, σ=π/8)
-
-q = STATsourceD(𝐩ₛ,p,beam,G)
-
-#q = STATsourceD(𝐩ₛ,p,𝐛,G)
+G(θ) = 𝒩ᵤ(θ, μ=0.0, σ=π/16)
+q = STATsourceD(𝐩ₛ,p,𝐛,G)
 r = pointReflector([𝛏₁,𝛏₂,𝛏₃,𝛏₄],[α₁,α₂,α₃,α₄],[q])
-
-z = STATreceiverD(r,𝐩ᵣ,beam,G)
-#z = STATreceiverD(r,𝐩ᵣ,𝐛,G)
-
+z = STATreceiverD(r,𝐩ᵣ,𝐛,G)
 t = -5.0e-9:1.0e-10:75.0e-9
 p1 = plot(t,p, xlab="time (sec)", ylab="p(t)", legend=:false)
 p2 = plot( t, z(t), xlab="time (sec)", ylab="z(t)", legend=:false)
@@ -48,40 +30,49 @@ png(path*"scenarioE_STATD.png")
 png(path*"scenarioESTAT_signal.png")
 
 
-
-
-
-function beamtime(t::Float64)
-    return ifelse(0.0<t<T,beam(t) , ifelse(T<t<2T, beam(t+T), ifelse(2T<t<3T, beam(t+2T), beam(t+3T))))
-end
-
-
-
-
-Dᵣ(ξ::Vector{Float64}) = G(angleBetween(beamtime((norm(ξ-𝐩ₛ) .+ norm(𝐩ᵣ-ξ))./c), ξ.-𝐩ᵣ))
-Dₛ(ξ::Vector{Float64}) = G(angleBetween(beamtime((norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c), ξ.-𝐩ₛ))
-
-#Dᵣ(ξ::Vector{Float64}) = G(angleBetween(𝐛((norm(ξ-𝐩ₛ) .+ norm(𝐩ᵣ-ξ))./c.-T), ξ.-𝐩ᵣ))
-#Dₛ(ξ::Vector{Float64}) = G(angleBetween(𝐛((norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c.-T), ξ.-𝐩ₛ))
-
 zₜ = PulseTrainReceivers(z,T)
 
+#function beam(t::Float64)
+#    return ifelse(0.0<t<T,𝐛(t) , ifelse(T<t<2T, 𝐛(T+t), ifelse(2T<t<3T, 𝐛(2T+t), 𝐛(3T+t))))
+#end
 
-f(ξ::Vector{Float64}) = (zₜ((norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c).*Dᵣ(ξ).*Dₛ(ξ))/
+
+#Dₛ(ξ::Vector{Float64}) = G(angleBetween(𝐛(3T+(norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c), ξ.-𝐩ₛ))
+
+#f(ξ::Vector{Float64}) = (zₜ((norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c).*Dₛ(ξ))/
+#                        (A(norm(ξ-𝐩ₛ)/c).*A(norm(𝐩ᵣ-ξ)/c))
+
+Dₛ1(ξ::Vector{Float64}) = G(angleBetween(𝐛((norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c), ξ.-𝐩ₛ))
+f1(ξ::Vector{Float64}) = (zₜ((norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c).*Dₛ1(ξ))/
                         (A(norm(ξ-𝐩ₛ)/c).*A(norm(𝐩ᵣ-ξ)/c))
 
-#f(ξ::Vector{Float64}) = (zₜ((norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c))/
-#                        (A(norm(ξ-𝐩ₛ)/c).*A(norm(𝐩ᵣ-ξ)/c))
+Dₛ2(ξ::Vector{Float64}) = G(angleBetween(𝐛(T+(norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c), ξ.-𝐩ₛ))
+f2(ξ::Vector{Float64}) = (zₜ((norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c).*Dₛ2(ξ))/
+                        (A(norm(ξ-𝐩ₛ)/c).*A(norm(𝐩ᵣ-ξ)/c))
+
+Dₛ3(ξ::Vector{Float64}) = G(angleBetween(𝐛(2T+(norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c), ξ.-𝐩ₛ))
+f3(ξ::Vector{Float64}) = (zₜ((norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c).*Dₛ3(ξ))/
+                        (A(norm(ξ-𝐩ₛ)/c).*A(norm(𝐩ᵣ-ξ)/c))
+
+Dₛ4(ξ::Vector{Float64}) = G(angleBetween(𝐛(3T+(norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c), ξ.-𝐩ₛ))
+f4(ξ::Vector{Float64}) = (zₜ((norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c).*Dₛ4(ξ))/
+                        (A(norm(ξ-𝐩ₛ)/c).*A(norm(𝐩ᵣ-ξ)/c))
+
+
+f(ξ::Vector{Float64}) = f1(ξ).+ f2(ξ) .+f3(ξ).+f4(ξ)
 
 inverse2Dplot([q],r,[z],f)
 
 
 
-png(path*"scenarioESTAT_simulation.png")
+p11 = inverse2Dplot([q],r,[z],f1)
+p12 = inverse2Dplot([q],r,[z],f2)
+p13 = inverse2Dplot([q],r,[z],f3)
+p14 = inverse2Dplot([q],r,[z],f4)
 
+plot(p11,p12,p13,p14,layout=(2,2))
 
-
-
+png(path*"scenarioESTAT_simulationaall2.png")
 
 
 
