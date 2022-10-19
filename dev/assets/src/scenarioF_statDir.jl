fd047e567f071da8c50e2a215f5c7c154b9b1708
Documenter.jl <documenter@juliadocs.github.io>
2022-10-19 06:41:55 +0000

    build based on 30687a6

--- dev/assets/src/scenarioF_statDir.jl
+++ dev/assets/src/scenarioF_statDir.jl
@@ -9,16 +9,16 @@ tₚ = 1.0e-06 # in microseconds
 T  = 15.0e-6
 p(t) = δn(mod(t-tₚ,T),1.0e-07)
 α₁ = 0.7; 𝛏₁ = [0.11c*T,-0.07c*T]
-α₂ = 0.7; 𝛏₂ = [-0.19c*T,0.0]
+α₂ = 0.7; 𝛏₂ = [0.09c*T,0.04c*T]
 α₃ = 0.7; 𝛏₃ = [0.22c*T,0.20c*T]
-f₀ = 1/4T
+f₀ = 1/T
 𝐛(t) = [cos(2π*f₀*(t-tₚ)),sin(2π*f₀*(t-tₚ))]
-G(θ) = 𝒩ᵤ(θ, μ=0.0, σ=π/12)
+G(θ) = 𝒩ᵤ(θ, μ=0.0, σ=π/16)
 q = STATsourceD(𝐩ₛ,p,𝐛,G)
 #r = pointReflector([𝛏₁,𝛏₂,𝛏₃,𝛏₄,𝛏₅],[α₁,α₂,α₃,α₄,α₅],[q])
 r = pointReflector([𝛏₁,𝛏₂,𝛏₃],[α₁,α₂,α₃],[q])
 z = STATreceiverD(r,𝐩ᵣ,𝐛,G)
-t=0.0:T/100:10T
+t=0.0:T/100:5T
 p1 = plot(t,p, xlab="time (sec)", ylab="p(t)", legend=:false)
 p2 = plot( t, z(t),ylims=(minimum(z(t)),maximum(z(t))), xlab="time (sec)", ylab="z(t)", legend=:false)
 plot(p1,p2,layout=(2,1))
@@ -32,6 +32,16 @@ png(path*"scenarioF_STATD.png")
 png(path*"scenarioFSTAT_signal.png")
 
 #zₜ = PulseTrainReceivers(z,T)
+
+
+M=10
+fm(ξ::Vector{Float64}) = [z(tₚ+(k-1)*T+(norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c).*G(angleBetween(𝐛(tₚ-(k-1)T), ξ.-𝐩ₛ))/(A(norm(ξ-𝐩ₛ)/c).*A(norm(𝐩ᵣ-ξ)/c)) for k∈1:M]
+g(ξ::Vector{Float64}) = sum(fm(ξ)[i] for i ∈ 1:M )
+
+inversePlot2D([q],r,[z],g,T)
+
+
+
 
 Dₛ1(ξ::Vector{Float64}) = G(angleBetween(𝐛(tₚ), ξ.-𝐩ₛ))
 Dₛ2(ξ::Vector{Float64}) = G(angleBetween(𝐛(tₚ-T), ξ.-𝐩ₛ))
@@ -55,16 +65,16 @@ f5(ξ::Vector{Float64}) = (z(tₚ+4T+(norm(ξ-𝐩ₛ).+ norm(𝐩ᵣ-ξ))./c).*
                           (A(norm(ξ-𝐩ₛ)/c).*A(norm(𝐩ᵣ-ξ)/c))                        
 
 f(ξ::Vector{Float64}) = f1(ξ).+ f2(ξ) .+f3(ξ).+f4(ξ).+f5(ξ)
-inverse2Dplot([q],r,[z],f)
+inversePlot2D([q],r,[z],f,T)
 
 
 
 
-p11 = inverse2Dplot([q],r,[z],f1)
-p12 = inverse2Dplot([q],r,[z],f2)
-p13 = inverse2Dplot([q],r,[z],f3)
-p14 = inverse2Dplot([q],r,[z],f4)
-p15 = inverse2Dplot([q],r,[z],f5)
+p11 = inversePlot2D([q],r,[z],f1,T)
+p12 = inversePlot2D([q],r,[z],f2,T)
+p13 = inversePlot2D([q],r,[z],f3,T)
+p14 = inversePlot2D([q],r,[z],f4,T)
+p15 = inversePlot2D([q],r,[z],f5,T)
 plot(p11,p12,p13,p14,p15,layout=(5,1),size=(2000,2000))
 
 scene2Dplot([q],r,[z])
