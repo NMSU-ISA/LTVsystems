d312d8d4b62b0b49677cd13601d7da2a992d2bcd
Documenter.jl <documenter@juliadocs.github.io>
2022-10-28 23:16:55 +0000

    build based on 83a9794

--- dev/assets/src/scenarioB_Dir.jl
+++ dev/assets/src/scenarioB_Dir.jl
@@ -3,34 +3,37 @@ path = "docs/src/assets/"
 using LTVsystems
 using Plots
 𝐩ₛ =  [0.0, 0.0]
-𝐩ᵣ =  [-1.0, 0.0]
-p(t) = δn(t,1.0e-10)
+𝐩ᵣ =  [1.5e-06c, 0.0]
+tₚ = 1.0e-06 # in microseconds
+p(t) = δn(t-tₚ,1.0e-07)
 𝐛 = [1.0,0.0]
-G(θ) = 𝒩ᵤ(θ, μ=0.0, σ=π/8)
+G(θ) = 𝒩ᵤ(θ, μ=0.0, σ=π/6)
 q = LTIsourceDTI(𝐩ₛ,p,𝐛,G)
-α₀ = 0.7; 𝛏₀ = [1.8,0.0]
+α₀ = -0.7; 𝛏₀ = [3.75e-06c,0.0]
 r = pointReflector(𝛏₀,α₀,q)
 z = LTIreceiverDTI([r],𝐩ᵣ,𝐛,G)
 #TEMPORAL SIMULATION
-t = 0.0:1.0e-10:25.5e-9
-plot( t, z(t), xlab="time (sec)", ylab="z(t)", legend=:false)
-
+t=0.0:1.0e-08:25.0e-06
+p1 = plot(t,p, xlab="time (sec)", ylab="p(t)", legend=:false)
+p2 = plot( t, z(t), xlab="time (sec)", ylab="z(t)", legend=:false)
+plot(p1,p2,layout=(2,1))
 
 png(path*"scenarioB_LTIDirsignal.png")
 
-scene2Dplot([q],[r],[z])
+scenePlot2D([q],[r],[z])
 
-scene2Ddirplot([q],[r],[z],𝐛)
+scenedirPlot2D([q],[r],[z],𝐛)
+
 png(path*"scenarioB_LTIDir.png")
 
 # Inverse modeling
 
 Dᵣ(ξ::Vector{Float64}) = G(angleBetween(𝐛, ξ.-𝐩ᵣ))
 Dₛ(ξ::Vector{Float64}) = G(angleBetween(𝐛, ξ.-𝐩ₛ))
-f(ξ::Vector{Float64}) = (z((norm(ξ-𝐩ₛ) .+ norm(𝐩ᵣ-ξ))./c).*Dₛ(ξ::Vector{Float64}).*Dᵣ(ξ::Vector{Float64}))/
+f(ξ::Vector{Float64}) = (z(tₚ+(norm(ξ-𝐩ₛ) .+ norm(𝐩ᵣ-ξ))./c).*Dₛ(ξ::Vector{Float64}).*Dᵣ(ξ::Vector{Float64}))/
                         (A(norm(ξ-𝐩ₛ)/c).*A(norm(𝐩ᵣ-ξ)/c))
 
 #SPATIAL SIMULATION
-inverse2Dplot([q],[r],[z],f)
+inversePlot2D([q],[r],[z],f)
 
 png(path*"scenarioB_DirTIsimulation.png")
