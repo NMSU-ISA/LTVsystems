4cdc0b2dfe7bd9e86cd80dbd0238302da17b8375
Mamta Dalal <mamta.dalal7@gmail.com>
2022-11-08 11:47:37 -0700

    update sources help function

--- src/stationaryDirection/sources.jl
+++ src/stationaryDirection/sources.jl
@@ -1,5 +1,22 @@
-#DEFINE STATIONARY SOURCE w/ DIRECTIONAL ANTENNA and TIME-VARYING BEAM CENTER
+"""
+    q = STATsourceD(𝐩ₛ, p)
+    
+Create a Stationary Directional Source by calling `STATsourceD()` with
+the *source position*, 𝐩ₛ ,a *transmitted signal*, ``\\mathsf{p}(t)``, *time-varying (rotating) beam center*,
+``\\bm{b}_\\mathrm{s}(t)`` and the *source antenna's gain*,
+``\\mathrm{G}_\\mathrm{s}(\\Theta).``
 
+# Examples
+```@example
+using LTVsystems
+𝐩ₛ =  [0.0, 0.0]
+tₚ = 1.0e-06
+p(t) = δn(t-tₚ,1.0e-07)
+𝐛ₛ(t) = [cos(2π*10*t),0.0]/(norm(cos(2π*10*t)))
+G(θ) = 𝒩ᵤ(θ, μ=0.0, σ=π/8)
+q = STATsourceD(𝐩ₛ, p, 𝐛ₛ, G)
+```
+"""
 struct STATsourceD <: Sources
   position::Vector{Float64}
   transmission ::Function
